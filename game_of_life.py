8adbb7cdcce782e2cbc315548a96172ccc2c1046
Maya Raz <mayaraz23420@gmail.com>
2020-05-05 23:54:11 +0300

    added all logic

---
+++ game_of_life.py
@@ -0,0 +1,98 @@
+from typing import List
+
+
+class Game:
+	DEAD = '0'
+	LIVE = 'X'
+
+	def __init__(self, board):
+		self.board = board
+
+	def _set_cell_state(self, i: int, j: int) -> None:
+		"""
+		Gets indexes of cell in the board and sets it's new life state according the game rules
+		:param i: row index
+		:param j: column index
+		:return: None
+		"""
+		neighbors = self._get_neighbors(i, j)
+		live_cells_amount = neighbors.count(self.LIVE)
+		if self.board[i][j] == self.LIVE:
+			self.board[i][j] = self.LIVE if 2 <= live_cells_amount <= 3 else self.DEAD
+		else:
+			self.board[i][j] = self.LIVE if live_cells_amount == 3 else self.DEAD
+
+	def _get_neighbors(self, i: int, j: int) -> List[str]:
+		"""
+		Gets cell indexes and returns list of it's neighbors values (life states)
+		:param i: row index
+		:param j: column index
+		:return: neighbors list (str list)
+		"""
+		from_row = i - 1 if i != 0 else 0
+		to_row = i + 2 if i != len(self.board) - 1 else len(self.board)
+
+		from_column = j - 1 if j != 0 else 0
+		to_column = j + 2 if j != len(self.board[j]) - 1 else len(self.board[j])
+
+		neighbors = []
+		for x in range(from_row, to_row):
+			for y in range(from_column, to_column):
+				if not (x == i and y == j):
+					neighbors.append(self.board[x][y])
+
+		return neighbors
+
+	def _run_next_iteration(self) -> None:
+		"""
+		Sets new life state of all cells in board
+		:return: None
+		"""
+		for i in range(len(self.board)):
+			for j in range(len(self.board[0])):
+				self._set_cell_state(i, j)
+
+	def print_board(self) -> None:
+		"""
+		prints current board
+		:return: None
+		"""
+		for row in self.board:
+			print(*row, sep='  ')
+
+		print('___' * len(self.board))
+
+	def run(self) -> None:
+		"""
+		Runs the game
+		:return: None
+		"""
+		print('Game started')
+		self.print_board()
+		continue_running = True
+		while continue_running:
+			self._run_next_iteration()
+			self.print_board()
+			continue_running = input('Continue to next generation? [y]/n\n') != 'n'
+
+		print('Finish game')
+
+
+def main():
+	game = Game(board=[
+		['0', 'X', '0', '0', '0', '0', '0', '0', '0', '0'],
+		['0', 'X', 'X', '0', '0', '0', '0', '0', '0', '0'],
+		['0', 'X', '0', '0', '0', '0', '0', '0', '0', '0'],
+		['0', '0', '0', 'X', '0', 'X', '0', '0', '0', '0'],
+		['0', '0', '0', '0', 'X', '0', '0', '0', '0', '0'],
+		['0', '0', '0', 'X', '0', 'X', '0', '0', '0', '0'],
+		['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+		['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+		['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+		['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+	])
+	game.run()
+
+
+if __name__ == '__main__':
+	main()
